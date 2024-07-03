9d35c705d701f21d36f2ff5c9c883f8aa7aff4ac
Luigi Marino <emails@luigi-marino.com>
2024-07-03 16:29:25 +0100

    Menu options added

--- Program.cs
+++ Program.cs
@@ -15,31 +15,67 @@
 
         public void Timer()
         {
-
-            while (timer < Time && !Globals.exitSwitch)
+            while (timer <= Time && !Globals.exitSwitch)
             {
-                Console.Write($"\r{timer}");
+                Console.Write($"\r{Title} - {timer}");
                 Thread.Sleep(5);
                 timer++;
             }
 
-            Status = timer == Time ? true: false;
+            Status = timer >= Time ? true: false;
+            SaveToHistory(Title, Time, Status);
         }
 
-        public void SaveToHistory()
+        public void SaveToHistory(string title, int time, bool status)
         {
-            return;
+            DateTime timeStamp = DateTime.Today;
+            string completed = status ? "Completed" : "Not Finished";
+            string csvEntry = $"\n{timeStamp.ToString("d")}, {title}, {time}, {completed}";
+            File.AppendAllText("history.csv", csvEntry);
         }
     }
 
     class Application
     {
+        static Focus currentFocus = new Focus();
 
         static void Main(string[] args)
         {
-            Focus currentFocus = new Focus();
 
-            if (args.Length == 0)
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(closeHandler);
+
+            if (args.Length != 0 && args[0].StartsWith("-"))
+            {
+                string menuOption = args[0].ToLower();
+                switch (menuOption)
+                {
+                    case "-help":
+                        menuHelp();
+                        break;
+
+                    case "-focus":
+                        menuFocus(args);
+                        currentFocus.Timer();
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Menu Input");
+                        break;
+                }
+            }
+            else
+                Console.WriteLine("Invalid Option - Type 'dotnet run -help' for further options");
+
+        }
+
+        static void menuFocus(string[] menu)
+        {
+            if (menu.Length == 3)
+            {
+                currentFocus.Title = menu[1];
+                currentFocus.Time = Int32.Parse(menu[2])*60;
+            }
+            else
             {
                 Console.Write($"Please enter a Focus Title: ");
                 currentFocus.Title = Console.ReadLine();
@@ -47,18 +83,24 @@
                 Console.Write($"Please enter a Focus Time: ");
                 currentFocus.Time = Int32.Parse(Console.ReadLine())*60;
             }
-            else
-            {
-                currentFocus.Title = args[0];
-                currentFocus.Time = Int32.Parse(args[1]);
-            }
-
-            Console.CancelKeyPress += new ConsoleCancelEventHandler(closeHandler);
-            currentFocus.Timer();
-
-            // Test Outputs
-            Console.WriteLine("\n---------------------------------");
-            Console.WriteLine($"Title: {currentFocus.Title}\nTime: {currentFocus.Time / 60} minutes\nCompleted: {currentFocus.Status}");
+        }
+        
+        static void menuHelp()
+        {
+            Console.WriteLine($"Focus Session Tracker - Command Line Tool\n");
+            Console.WriteLine(@"Usage:
+               focus-session [options]");
+            Console.WriteLine(@"Options:
+               -h,  -help      Show the help page for this application
+               -f,  -focus     Start a new focus session
+               -v,  -version   Show the current application version");
+            Console.WriteLine(@"Examples:
+               focus-tracker -focus ""example_title"" 10");
+            Console.WriteLine(@"Description:
+                The Focus Session Tracker is a command line tool designed to help you track your focus sessions. 
+                By specifying a session title and duration, you can start a timer to keep you on track with your tasks. 
+                When the session is complete, you will receive a notification.");
+            Console.WriteLine("");
         }
 
         void Reporting(Focus focus)
--- obj/Debug/net8.0/FocusTimer.AssemblyInfo.cs
+++ obj/Debug/net8.0/FocusTimer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FocusTimer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68fc5baecea8d64292660ece4b16bee80b9a899a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FocusTimer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FocusTimer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
